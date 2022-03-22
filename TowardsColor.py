0e19c731b69a9c59d90148ba95b133a42fee5ada
Kinnucan <kinnucanr@gmail.com>
2022-03-22 18:02:11 -0500

    Changed the target color for TowardsColor.py

--- TowardsColor.py
+++ TowardsColor.py
@@ -5,7 +5,7 @@ class TowardsColor:
     def __init__(self, obj):
         self.robot = obj
         self.prevHead = 0
-        self.targetColor = 0
+        self.targetColor = 5
         self.targetFound = False
 
     def run(self):
@@ -13,6 +13,7 @@ class TowardsColor:
             return 30.0, 0.0
         else:
             currentColor = self.robot.readColor()
+            print(currentColor)
             if currentColor == self.targetColor:
                 self.targetFound = True
                 return 30.0, 0.0
