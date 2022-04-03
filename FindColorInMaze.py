45518abf9c70a19ab5b0481ff75c6ee976a3b863
Kinnucan <ryan.kinnucan@gtt.com>
2022-04-03 13:02:14 -0500

    Trying new approach for Task 3

--- FindColorInMaze.py
+++ FindColorInMaze.py
@@ -19,31 +19,48 @@ config = {
 robot = SturdyBot("Maze Escaper", config)
 
 def run():
-    brain = PotentialFieldBrain.PotentialFieldBrain(robot)
+    # brain = PotentialFieldBrain.PotentialFieldBrain(robot)
 
-    towardsColor = TowardsColor(robot)
-    obstacleForce_straight = ObstacleForceV2(robot, 0)
-    # obstacleForce_right_angle = ObstacleForceV2(robot, 45)
-    # obstacleForce_right = ObstacleForceV2(robot, 90)
-    # obstacleForce_left_angle = ObstacleForceV2(robot, -45)
-    # obstacleForce_left = ObstacleForceV2(robot, -90)
+    # towardsColor = TowardsColor(robot)
+    # obstacleForce_straight = ObstacleForceV2(robot, 0)
+    # # obstacleForce_right_angle = ObstacleForceV2(robot, 45)
+    # # obstacleForce_right = ObstacleForceV2(robot, 90)
+    # # obstacleForce_left_angle = ObstacleForceV2(robot, -45)
+    # # obstacleForce_left = ObstacleForceV2(robot, -90)
 
-    brain.add( towardsColor )
-    brain.add( obstacleForce_straight )
-    # brain.add( obstacleForce_right_angle )
-    # brain.add( obstacleForce_right )
-    # brain.add( obstacleForce_left )
-    # brain.add( obstacleForce_left_angle )
+    # brain.add( towardsColor )
+    # brain.add( obstacleForce_straight )
+    # # brain.add( obstacleForce_right_angle )
+    # # brain.add( obstacleForce_right )
+    # # brain.add( obstacleForce_left )
+    # # brain.add( obstacleForce_left_angle )
+
+    # colorNotFound = True
+    # while colorNotFound:
+    #     try:
+    #         brain.step()
+    #         if (robot.readColor == 1 and robot.readDistance <= 10):
+    #             colorNotFound = False
+    #     except KeyboardInterrupt:
+    #         brain.stopAll()
+    #         sys.exit(0)
+    # brain.stopAll()
 
     colorNotFound = True
     while colorNotFound:
         try:
-            brain.step()
-            if (robot.readColor == 1 and robot.readDistance <= 10):
+            robot.forward(20.0, 5.0)
+            if (robot.readDistance <= 10):
+                rightDistance = robot.turnRight(30, (90.0 / 180.0))
+                print(rightDistance)
+                leftDistance = robot.turnLeft(30, (180.0 / 180.0))
+                print(leftDistance)
+                if (rightDistance < leftDistance):
+                    robot.turnRight(30, (180.0 / 180.0))
+            if (robot.readColor == 5):
                 colorNotFound = False
-        except KeyboardInterrupt:
-            brain.stopAll()
-            sys.exit(0)
-    brain.stopAll()
+        except:
+            print("Robot Stopped")
+    print("Color Found!")
 
 run()
